5c4bc6fd51d26e4e041c8c746df7482858494ba4
Tri <triminhnguyen.dev@gmail.com>
2025-06-04 12:14:34 +0700

    feat: add authentication layout and sign-in/sign-up pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+function page() {
+  return (
+    <Card className="w-full max-w-md shadow-xl">
+      <CardHeader>
+        <CardTitle className="text-center text-2xl font-bold">
+          Welcome
+        </CardTitle>
+      </CardHeader>
+      <CardContent>sign in page</CardContent>
+    </Card>
+  );
+}
+
+export default page;
