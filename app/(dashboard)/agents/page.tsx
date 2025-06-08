f8ac6cc7299f2f3493c72daca144dac48c5460b9
Tri <triminhnguyen.dev@gmail.com>
2025-06-08 18:12:45 +0700

    refactor(agent): move AgentPage component from page.tsx to AgentPage.tsx

--- app/(dashboard)/agents/page.tsx
+++ app/(dashboard)/agents/page.tsx
@@ -1,35 +1,12 @@
-"use client";
-import { useQuery } from "@tanstack/react-query";
-
-import { useTRPC } from "@/trpc/client";
-import LoadingState from "@/components/LoadingState";
-import ErrorState from "@/components/ErrorState";
+import AgentPage from "@/components/agent/AgentPage";
+import React from "react";
 
 function Page() {
-  const trpc = useTRPC();
-  const { data, isLoading, isError } = useQuery(
-    trpc.agent.getMany.queryOptions()
+  return (
+    <div>
+      <AgentPage />
+    </div>
   );
-
-  if (isLoading) {
-    return (
-      <LoadingState
-        title="Loading Agents"
-        describtion="Please wait while we fetch the latest list of agents."
-      />
-    );
-  }
-
-  if (isError) {
-    return (
-      <ErrorState
-        title="Failed to Load Agents"
-        description="We encountered an error while trying to fetch the list of agents. Please check your internet connection or try again later."
-      />
-    );
-  }
-
-  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
 
 export default Page;
---
+++ components/agent/AgentPage.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import LoadingState from "@/components/LoadingState";
+import ErrorState from "@/components/ErrorState";
+
+function AgentPage() {
+  const trpc = useTRPC();
+  const { data, isLoading, isError } = useQuery(
+    trpc.agent.getMany.queryOptions()
+  );
+
+  if (isLoading) {
+    return (
+      <LoadingState
+        title="Loading Agents"
+        describtion="Please wait while we fetch the latest list of agents."
+      />
+    );
+  }
+
+  if (isError) {
+    return (
+      <ErrorState
+        title="Failed to Load Agents"
+        description="We encountered an error while trying to fetch the list of agents. Please check your internet connection or try again later."
+      />
+    );
+  }
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+}
+
+export default AgentPage;
